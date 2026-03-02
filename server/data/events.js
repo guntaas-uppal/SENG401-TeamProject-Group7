2bf0c6d01eb1ce1d2580b611b68f8da7d54c352b
rajdeeepdas <rajdeep.das@ucalgary.ca>
2026-03-02 09:05:37 +0600

    added events.js index and all household scenario events

---
+++ server/data/events.js
@@ -0,0 +1,13 @@
+
+const household = require("./householdEvents.js");
+const city = require("./cityEvents");
+const country = require("./countryEvents");
+
+const events = {
+household,
+city,
+country,
+};
+
+module.exports = events;
+
